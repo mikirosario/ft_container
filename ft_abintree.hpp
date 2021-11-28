5f97c4bf7954a22513e2026659a027e6712ce122
Miki Rosario <chisparroja@gmail.com>
2021-11-28 04:19:32 +0100

    All binary tree functions except the insert functions, element access bracket overload and node key and value pointer setters have been moved to the abstract class, leaving the derived classes much cleaner.

--- ft_abintree.hpp
+++ ft_abintree.hpp
@@ -6,7 +6,7 @@
 /*   By: miki <miki@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/21 14:13:06 by miki              #+#    #+#             */
-/*   Updated: 2021/11/28 03:28:09 by miki             ###   ########.fr       */
+/*   Updated: 2021/11/28 04:06:30 by miki             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,6 +69,12 @@ namespace ft
 	class Abintree
 	{
 		public:
+			/*
+			** I find it really confusing that the mapped_type is the pair value
+			** type while the value_type is the pair type, so I use data_type as
+			** a synonym for value_type so I don't confuse it with the
+			** mapped_type.
+			*/
 			typedef Data	data_type;
 			typedef Data	value_type;
 			typedef Key		key_type;
@@ -275,6 +281,7 @@ namespace ft
 
 			/* CONSTRUCTORS AND DESTRUCTOR */
 			Abintree(void) : _root(NULL), _min(NULL), _max(NULL), _size(0) {}
+			virtual ~Abintree(void) {}
 
 			/* ---- PROTECTED BINARY TREE CONTROL FUNCTIONS ---- */
 			/*
@@ -725,7 +732,7 @@ namespace ft
 				{
 					//get successor node, swap data with it, and delete the successor
 					t_bstnode * successor = node->right;
-					value_type	data;
+					data_type	data;
 					while (successor->left != NULL)
 						successor = successor->left;
 					data = node->data;
@@ -1277,6 +1284,256 @@ namespace ft
 					return (0);
 				else
 					return (1 + recursive_count(node->right, key) + recursive_count(node->left, key));
+			}
+		public:
+			/* ---- ITERATORS ---- */
+
+			/* BEGIN AND END */
+			/*
+			** These functions respectively return iterators to the first
+			** element of the container and the memory address after the last
+			** element of the container. Const containers return
+			** const_iterators, which are actually iterators TO const values
+			** (the iterators themselves can be modified).
+			**
+			** In the binary tree, the address 'after' the last element and
+			** 'before' the first element is virtualized to NULL, which is not
+			** resolvable.
+			**
+			** Each iterator also stores its last resolvable address, so that if
+			** incremented OR decremented from NULL it will return to its last
+			** address.
+			**
+			** Note: If iterators are decremented or incremented past NULL, the
+			** iterator will infinitely toggle between the last address and NULL
+			** until the direction is reversed! So rather than the typical
+			** segmentation fault for iterating out of bounds, you may also get
+			** an infinite loop. Avoiding this behaviour would involve extra
+			** overhead in the iterator and, frankly, I think the binary tree
+			** has more than enough overhead as it is. ;)
+			**
+			** Note: While technically these are bidirectional iterators, full
+			** functionality is implemented.
+			*/
+			iterator begin(void) {
+				return (iterator(_min));
+			}
+			const_iterator begin(void) const {
+				return (const_iterator(_min));
+			}
+			iterator end(void) {
+				return (iterator(_max) + 1);
+			}
+			const_iterator end(void) const {
+				return (const_iterator(_max) + 1);
+			}
+
+			iterator rbegin(void) {
+				return (iterator(_max) + 1);
+			}
+			const_iterator rbegin(void) const {
+				return (const_iterator(_max) + 1);
+			}
+			iterator rend(void) {
+				return (iterator(_min));
+			}
+			const_iterator rend(void) const {
+				return (const_iterator(_min));
+			}
+
+			/* ---- CAPACITY ---- */
+
+			/* SIZE */
+			/*
+			** This method returns the number of ELEMENTS in the container, NOT
+			** its allocated memory!
+			*/
+			size_type	size(void) const {
+				return(_size);
+			}
+
+			/* EMPTY */
+			/*
+			** This method returns true if the container is empty. Otherwise it
+			** returns false.
+			*/
+			bool		empty(void) const {
+				return(_size == 0);
+			}
+
+			/* MAX_SIZE */
+			/*
+			** This method returns the container's maximum size, for which we
+			** use its allocator's maximum size.
+			*/
+			size_type			max_size(void) const {
+				return (_alloc.max_size());
+			}
+
+			void		erase(t_bstnode & node) {
+				this->bintree_delete(&node);
+			}
+
+			void		erase(iterator position) {
+				erase(&(*position));
+			}
+
+			void		clear(void) {
+				this->_root = bintree_free(this->_root);
+			}
+
+			t_bstnode &	getRootNode(void) const {
+				return (_root);
+			}
+			t_bstnode &	getNode(key_type const & key) const {
+				return (*(bintree_search(_root, key)));
+			}
+
+			/* LOWER BOUND */
+			/*
+			** This function first obtains the node containing an exact match to
+			** 'key' (if one exists), or else the node that would be closest to
+			** a node containing 'key', from the getNearestNode function, and
+			** creates an iterator out of it. If the nearest_node key is less
+			** than 'key', the iterator is incremented by one for the next
+			** highest key. If the nearest_node key is greater than or equal to
+			** 'key', then it is returned directly.
+			**
+			** -- RETURN VALUE --
+			** The returned iterator points to the node considered to come AFTER
+			** a node containing 'key', or to the first node EQUAL to 'key' if
+			** one exists.
+			*/
+			iterator		lower_bound(key_type const & key) {
+				t_bstnode *		nearest_node = getNearestNode(_root, NULL, key);
+				iterator		ret(nearest_node);
+
+				if (C_key(*nearest_node->key) < C_key(key))
+					++ret;
+				return (ret);
+			}
+
+			const_iterator	lower_bound(key_type const & key) const {
+				t_bstnode *		nearest_node = getNearestNode(_root, NULL, key);
+				const_iterator	ret(nearest_node);
+
+				if (C_key(*nearest_node->key) < C_key(key))
+					++ret;
+				return (ret);
+			}
+
+			/* UPPER BOUND */
+			/*
+			** This function first obtains the node containing an exact match to
+			** 'key' (if one exists), or else the node that would be closest to
+			** a node containing 'key', from the getNearestNode function, and
+			** creates an iterator out of it. If the nearest_node key is less
+			** than or equal to 'key', the iterator is incremented by one for
+			** the next highest key. If the nearest_node key is greater than
+			** 'key', then it is returned directly.
+			**
+			** -- RETURN VALUE --
+			** The returned iterator points to the node considered to come AFTER
+			** a node containing 'key'.
+			*/
+			iterator		upper_bound(key_type const & key) {
+				t_bstnode * nearest_node = getNearestNode(_root, NULL, key);
+				iterator	ret(nearest_node);
+
+				if (C_key(*nearest_node->key) <= C_key(key))
+					++ret;
+				return(ret);
+			}
+
+			const_iterator	upper_bound(key_type const & key) const {
+				t_bstnode * 	nearest_node = getNearestNode(_root, NULL, key);
+				const_iterator	ret(nearest_node);
+
+				if (C_key(*nearest_node->key) <= C_key(key))
+					++ret;
+				return(ret);
+			}
+			
+			/* EQUAL RANGE */
+			/*
+			** This function returns the bounds of a range containing all nodes
+			** with a key equal to 'key', wherein the first iterator points to
+			** the beginning of the range and the second iterator points past
+			** the end of the range. This tree implementation only admits unique
+			** keys, so at most the range will consist of one node (the one
+			** pointed to by the first iterator).
+			**
+			** If no match exists for 'key' then both iterators will point to
+			** the node that would come after 'key'.
+			**
+			** The idea here is, imagine that you have a dictionary and one of
+			** the keys is 'light', with multiple possible definitions. So you
+			** have multiple 'lights' (not in a map, since it only admits unique
+			** keys, but maybe in a multimap). For example:
+			**
+			** KEY		VAL
+			** light	electromagnetic wave
+			** light	elementary particle
+			** light	luminous, bright
+			** light	low in weight
+			** lot		plot of land
+			**
+			** So, lower_bound("light") will return the first 'light',
+			** upper_bound("light") will return 'lot', and now you can iterate
+			** through the range, like so:
+			**
+			** for (it = lower_bound("light"), end = upper_bound("light"); it != end; ++it)
+			**	do_stuff
+			**
+			** Or using for_each, if you're into that sort of thing, of course.
+			** I happen to enjoy writing for loops. ;)
+			**
+			** In other words, you have a range of equal values - an
+			** 'equal_range'.
+			**
+			** That's probably what you want the *_bound functions for, so the
+			** equal_range function just directly retrieves that range for you
+			** and sticks it in a pair with two iterators.
+			**
+			** This is all explained terribly in the reference manuals, because
+			** C++ is all about making simple things simple. :p
+			**
+			** -- RETURN VALUE --
+			** A pair of iterators, respectively the lower_bound and upper_bound
+			** of the 'key' argument.
+			*/
+			ft::pair<iterator, iterator>				equal_range(key_type const & key) {
+				return (ft::pair<iterator, iterator>(lower_bound(key), upper_bound(key)));
+			}
+
+			ft::pair<const_iterator, const_iterator>	equal_range(key_type const & key) const {
+				return (ft::pair<const_iterator, const_iterator>(lower_bound(key), upper_bound(key)));
+			}
+
+			/* COUNT */
+			/*
+			** Returns the number of elements containing the key passed as key.
+			** For this tree this will always be either 0 or 1 as duplicates are
+			** not allowed, but a private recursive counting function is called
+			** that will also work with multimap trees. If I ever make one. ;p
+			**
+			** -- RETURN VALUE --
+			** The number of elements containing the 'key' passed as argument.
+			*/
+			size_type	count(key_type const & key) const {
+				return (recursive_count(_root, key));
+			}
+
+			/* FIND */
+			/*
+			** -- RETURN VALUE --
+			** Returns an iterator to the node with a key equal to the argument
+			** passed as 'key'. If no such node exists, returns the end
+			** iterator.
+			*/
+			iterator	find(key_type const & key) {
+				t_bstnode *	node = bintree_search(_root, key);
+				return (node == NULL ? end() : iterator(node));
 			}
 	};
 }
